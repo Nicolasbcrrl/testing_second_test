1ff3c2e71a61e0f7aafe7b735c1e105a4529c1e3
NicolasB <nicolas.becerra@etu.hesge.ch>
2022-10-30 13:58:13 +0100

    test update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import'@testing-library/jest-dom/extend-expect';
+import TodoTable from'./TodoTable';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders todotable', () => {
+  const row = [
+    {desc: 'Go to coffee', date: '30.10.2022'},
+  ];
+  render(<TodoTable todos={row} />);
+  const tablecell = screen.getByText(/Go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
 });
+
+test('add todo', ()=>{
+  render(<App/>)
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: {value:'Go to coffee'}
+  });
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: {value: '30.01.2021'}
+  });
+  const button = screen.getByText('Add');
+  fireEvent.click(button);
+  const tablecell = screen.getByText(/go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
+})
